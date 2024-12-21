1dd51a4d47c7bf5bdbfaf587e6427d80c2ba4639
Sav <sav.procopio@gmail.com>
2024-12-21 20:22:21 +0100

    Removed unused file

--- data_analyzing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Load the .npy file
-file_path = '/zhome/21/3/204026/DL-2/EHR_Mamba_model_89/P12data/split_1/test_physionet2012_1.npy'  # Replace with your file path
-data = np.load(file_path)
-
-# Display the contents
-print(data)
